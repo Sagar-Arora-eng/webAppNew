25a0a37bbcc6c929afa7fc6d127b3a6974789ee8
Sagar Arora <sagararor403@gmail.com>
2024-08-12 22:50:58 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+    user: 'bootcamp',
+    password: 'Pass@123',
+    server: 'bootcampaug5server.database.windows.net',
+    database: 'bootcampaug5db',
+    options: {
+      encrypt: true, // Use this if you're on Windows Azure
+      enableArithAbort: true
+    }
+  };
+  
+  module.exports = config;
+  
